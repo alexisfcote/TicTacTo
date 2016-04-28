fcdfb609e139e09c69d96b5f1eff15078949727e
Alexis <alexisfcote@gmail.com>
2016-04-27 21:44:46 -0400

    Initial commit

---
+++ src/tictacto/TicTacTo.java
@@ -0,0 +1,8 @@
+package tictacto;
+
+public class TicTacTo {
+	int[][] grid;
+	
+	public void play(int x, int y, int player){
+	}
+}
---
+++ src/tictacto/TicTacToController.java
@@ -0,0 +1,10 @@
+package tictacto;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class TicTacToController {
+
+	@FXML public void processNumpad(ActionEvent event) {}
+	
+}
